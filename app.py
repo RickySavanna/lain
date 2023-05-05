a483ddb759fdc70a60524d13afe4995b47d5f5d7
lain <rickyvsavanna@gmail.com>
2023-05-04 19:40:24 -0500

    termss

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from gpt3 import generate_response
 
 app = Flask(__name__)
 
-@app.route('/home')
+@app.route('/')
 def home():
     return render_template('home.html')
-
+    
 @app.route('/terms')
 def terms():
     return render_template('terms.html')
